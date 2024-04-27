96deadefa7342ddebe8677ea0aacb592bae74b85
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2024-04-27 17:37:59 +0530

    adding the configs

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,38 +1,63 @@
 "use client";
-import Image from 'next/image'
-import React from 'react'
-import {motion} from 'framer-motion'
-import {BsArrowRight, BsLinkedin, BsDownload} from 'react-icons/bs'
-import { FaGithubSquare } from 'react-icons/fa';
-import Link from 'next/link'
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  return (
-    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
-        <div className='flex items-center justify-center'>
-            <div className='realtive'>
-              <motion.div
-              initial={{opacity:0,scale:0}}
-              animate={{opacity:1,scale:1}}
-              transition={{
-                type:'tween',
-                duration:0.2,
-              }}>
-                <Image src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                       alt='Ravindu'
-                       width="192"
-                       height="192"
-                       quality="95"
-                       className='h-24 w-24 rounded-full border-[0.35rem] object-cover border-white shadow-xl'></Image>
-{/* 
-                       <span className='absolute bottom-0 right-0 text-4xl'>
-                        🖐
-                       </span> */}
-              </motion.div>
-            </div>
-        </div>
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
-        <motion.h1
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Ricardo portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -43,7 +68,6 @@ export default function Intro() {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
-
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -57,7 +81,8 @@ export default function Intro() {
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
-
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{" "}
@@ -70,7 +95,7 @@ export default function Intro() {
           download
         >
           Download CV{" "}
-          <BsDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
         <a
@@ -89,8 +114,6 @@ export default function Intro() {
           <FaGithubSquare />
         </a>
       </motion.div>
-
-      
     </section>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const { hostname } = require('os')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            }
-        ]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
